dd7c58d357178f835676b56a49554c7e64b9077a
SteveCmd <murimi101@gmail.com>
2024-11-06 15:33:56 +0300

    0. Regex-ing

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,16 +1,20 @@
-import logging
+#!/usr/bin/env python3
+"""
+Module for filtering sensitive information from log messages
+"""
+import re
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+def filter_datum(fields, redaction, message, separator):
+    """
+    Returns the log message obfuscated
+    Args:
+        fields: list of strings representing fields to obfuscate
+        redaction: string to replace sensitive info with
+        message: string representing the log line
+        separator: string representing the character separating fields
+    Returns:
+        The obfuscated log message
+    """
+    pattern = f'({"|".join(fields)})=[^{separator}]*'
+    return re.sub(pattern, f'\\1={redaction}', message)
