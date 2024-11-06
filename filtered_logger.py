358fa39a5a19a916b03379ce01dbb42a11a87a01
SteveCmd <murimi101@gmail.com>
2024-11-06 15:50:21 +0300

    1. Log formatter

--- filtered_logger.py
+++ filtered_logger.py
@@ -3,6 +3,7 @@
 Module for filtering sensitive information from log messages
 """
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -18,3 +19,21 @@ def filter_datum(fields, redaction, message, separator):
     """
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: tuple):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
