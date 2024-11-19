bf7369f3127d089cba34c126dc5a8b935f77816e
SteveCmd <murimi101@gmail.com>
2024-11-19 16:12:38 +0300

    16. Generate reset password token: Add method to generate reset password token for users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -131,3 +131,24 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generates a reset password token for a user.
+
+        Args:
+            email (str): The email address of the user.
+
+        Returns:
+            str: The reset password token.
+
+        Raises:
+            ValueError: If the user does not exist.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError("User does not exist")
